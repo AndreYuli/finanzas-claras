46f30b0b6c773871db45a96e07c54b121fe48bce
AndreYuli <126515557+AndreYuli@users.noreply.github.com>
2025-08-06 07:52:02 -0500

    feat: add form to create new transactions.

--- frontend/finanzas-claras/src/App.jsx
+++ frontend/finanzas-claras/src/App.jsx
@@ -3,6 +3,38 @@ import './App.css'
 
 function App() {
   const [transactions, setTransactions] = useState([])
+const [description, setDescription] = useState('')
+const [amount, setAmount] = useState('')
+const [type, setType] = useState('')
+
+const handleSubmit = (event) => {
+  event.preventDefault();
+  const newTransaction = {
+    description,
+    amount: parseFloat(amount),
+    type,
+    date: new Date().toISOString(),
+    category: 'General'
+  };
+
+  fetch('http://localhost:3001/api/transactions', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(newTransaction),
+  })
+    .then(response => response.json())
+    .then(savedTransaction => {
+      setTransactions([...transactions, savedTransaction]);
+      setDescription('');
+      setAmount('');
+      setType('');
+    })
+    .catch(error => console.error('Error al crear la transacción:', error));
+};
+
+
 
   useEffect(() => {
     const apiUrl = 'http://localhost:3001/api/transactions';
@@ -20,7 +52,40 @@ function App() {
       return (
         <>
         <h1>FinanzasClaras</h1>
-        <h2>Mi lista de transacciones</h2>
+
+        <form onSubmit={handleSubmit}>
+          <h3>Añadir nueva transacción</h3>
+          <div>
+            <label>Descripción:</label>
+            <input 
+            type = "text"
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+            required 
+            />
+          </div>
+          <div>
+            <label>Monto:</label>
+            <input 
+            type="number"
+            value={amount}
+            onChange={(e) => setAmount(e.target.value)}
+            required 
+            />
+          </div>
+          <div>
+            <label>Tipo:</label>
+            <select 
+            value={type}
+            onChange={(e) => setType(e.target.value)}
+            required>
+              <option value="">Seleccione un tipo</option>
+              <option value="EXPENSE">Gasto</option>
+              <option value="INCOME">Ingreso</option>
+            </select>
+          </div>
+          <button type="submit">Añadir transacción</button>
+        </form>
 
         <ul>
           {transactions.map(tx =>(
